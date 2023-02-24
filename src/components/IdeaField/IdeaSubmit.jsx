d8f551b76503cef5938bf7ba4ec394fc3ae5cdcc
Gabriel <gabrielalbernaz22@gmail.com>
2023-02-24 01:29:42 -0300

    Fix arrow idea field

--- src/components/IdeaField/IdeaSubmit.jsx
+++ src/components/IdeaField/IdeaSubmit.jsx
@@ -1,10 +1,11 @@
 import styles from './IdeaField.module.css'
 import React from 'react'
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 const IdeaSubmit = () => {
   return (
     <button className={styles.ideaSubmit}>
-        <i className="fa-solid fa-arrow-right"></i>
+       <AiOutlineArrowRight/>
     </button>
   )
 }
